14112878a273a5dcedca1fb5c9e0a700fa04c431
MercuryHomebase <zills.joshua@gmail.com>
2019-09-26 21:11:13 -0400

    changed background file paths

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,29 +2,30 @@ const fleetData = require("../data/fleetData");
 
 const availableData = require("../data/availableListData");
 
-module.exports = function(app) {
-    
-    app.get("/api/fleet", function(req, res) {
-        res.json(fleetData);
-      });
-    
-      app.get("/api/available", function(req, res) {
-        res.json(availableData);
-      });
+module.exports = function (app) {
+
+  app.get("/api/fleet", function (req, res) {
+    res.json(fleetData);
+  });
+
+  app.get("/api/available", function (req, res) {
+    res.json(availableData);
+  });
 
 
-    app.post("/api/fleet", function(req, res) {
-        
+  app.post("/api/fleet", function (req, res) {
+    console.log("fleetData.carStatus is " + fleetData.carStatus);
+    console.log("req.body.carStatus is " + req.body.carStatus);
+    if (fleetData.carStatus == "rented") {
+      fleetData.push(req, res);
+      res.json(true);
+    }
+    else {
+      availableData.push(req.body);
+      res.json(false);
+    }
+  });
 
-        if (fleetData.carStatus == "rented") {
-          fleetData.push(req,res);
-          res.json(true);
-        }
-        else {
-          availableData.push(req.body);
-          res.json(false);
-        }
-      });
 
-    
+
 }
